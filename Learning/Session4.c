84cbb01f31e057ea877dd9d36d9c8a9de2dcfd8e
BjoernGaul <153178070+BjoernGaul@users.noreply.github.com>
2024-03-07 11:22:34 +0100

    Session4 added

---
+++ Learning/Session4.c
@@ -0,0 +1,36 @@
+int main(){
+    char inp;
+
+     while(1){
+        printf("***************************\n");
+        printf("+       ----Menue----     +\n");
+        printf("+ Datei (d)               +\n");
+        printf("+ Ausschneiden (a)        +\n");
+        printf("+ Einfuegen (v)           +\n");
+        printf("+ Beenden (x)             +\n");
+        printf("+                         +\n");
+        printf("***************************\n");
+        printf("Eingabe: \n");
+        
+        scanf("%c%*c", &inp);
+        
+        if(inp == 'd'){
+            printf("Du hast Datei ausgewählt.\n");
+            continue;
+        }else if(inp == 'a'){
+            printf("Du hast Ausschneiden ausgewählt.\n");
+            continue;
+        }else if(inp == 'v'){
+            printf("Du hast Einfügen ausgewählt.\n");
+            continue;
+        }else if(inp == 'x'){
+            printf("Du hast Beenden ausgewählt.\n");
+            break;
+        }else{
+            printf("Keine gültige Eingabe!\n");
+            continue;
+        }
+        } 
+        
+     return 0;
+}
